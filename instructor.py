75cd2ffacb76162f0c237e7f57f1865be9af06e4
Bito Mann <bito@bitomann.com>
2020-05-01 12:02:49 -0500

    p1-2

--- instructor.py
+++ instructor.py
@@ -1,11 +1,22 @@
-class Instructor:
-    def __init__(self, first_name, last_name, slack_handle, cohort, specialty):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.slack_handle = slack_handle
-        self.cohort = cohort
-        self.specialty = specialty
+from nss_person import NSSPerson
 
-    def assign_exercise(self, exercise, student):
-        student.exercises.append(exercise)
-        print(f"Instructor: {self.first_name} \n Student {student.first_name} \n Assignment: {exercise.name}")
+class Instructor(NSSPerson):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.specialty = ""
+
+    def assign_exercise(self, student, exercise):
+        student.current_exercises.append(exercise)
+
+
+# class Instructor:
+#     def __init__(self, first_name, last_name, slack_handle, cohort, specialty):
+#         self.first_name = first_name
+#         self.last_name = last_name
+#         self.slack_handle = slack_handle
+#         self.cohort = cohort
+#         self.specialty = specialty
+
+#     def assign_exercise(self, exercise, student):
+#         student.exercises.append(exercise)
+#         print(f"Instructor: {self.first_name} \n Student {student .first_name} \n Assignment: {exercise.name}")
